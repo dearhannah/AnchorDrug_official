c88ac1e461294019d85f7153e6d7a47d676ae7de
han <miss.menghan@gmail.com>
2024-02-05 14:43:12 -0500

    anchor drug for every cell line exp update

--- ActiveLearning_one_cellline/c1.sh
+++ ActiveLearning_one_cellline/c1.sh
@@ -1,2 +1,6 @@
-python ActiveLearning.py -a=LeastConfidence -b=5 -q=100 -g=0
-python ActiveLearning.py -a=MarginSampling -b=5 -q=100 -g=1
+python ActiveLearning.py -a=LeastConfidence -b=5 -q=100 -c MCF7 -g=0
+python ActiveLearning.py -a=MarginSampling -b=5 -q=100 -c MCF7 -g=1
+python ActiveLearning.py -a=LeastConfidence -b=5 -q=100 -c A549 -g=0
+python ActiveLearning.py -a=MarginSampling -b=5 -q=100 -c A549 -g=1
+python ActiveLearning.py -a=LeastConfidence -b=5 -q=100 -c PC3 -g=0
+python ActiveLearning.py -a=MarginSampling -b=5 -q=100 -c PC3 -g=1
--- ActiveLearning_one_cellline/c2.sh
+++ ActiveLearning_one_cellline/c2.sh
@@ -1,2 +1,6 @@
-python ActiveLearning.py -a=KMeansSampling -b=5 -q=100 -g=2
-python ActiveLearning.py -a=KCenterGreedy -b=5 -q=100 -g=3
+python ActiveLearning.py -a=KMeansSampling -b=5 -q=100 -c MCF7 -g=2
+python ActiveLearning.py -a=KCenterGreedy -b=5 -q=100 -c MCF7 -g=3
+python ActiveLearning.py -a=KMeansSampling -b=5 -q=100 -c A549 -g=2
+python ActiveLearning.py -a=KCenterGreedy -b=5 -q=100 -c A549 -g=3
+python ActiveLearning.py -a=KMeansSampling -b=5 -q=100 -c PC3 -g=2
+python ActiveLearning.py -a=KCenterGreedy -b=5 -q=100 -c PC3 -g=3
