ea37b1dff8a66f4daed0d1fdbe4a9eb685ee6ba2
han <miss.menghan@gmail.com>
2024-07-03 23:45:10 -0400

    baseline

--- ActiveLearning_one_cellline/parameters.py
+++ ActiveLearning_one_cellline/parameters.py
@@ -12,7 +12,7 @@ args_pool = {'LINCS':
                  'transform_train': 'no',
 				 'transform': 'no',
 				 'loader_tr_args':{'batch_size': 32, 'num_workers': 4},
-				 'loader_te_args':{'batch_size': 64, 'num_workers': 4},
+				 'loader_te_args':{'batch_size': 256, 'num_workers': 4},
 				 'num_class':3,
 				 'optimizer':'Adam',
 				 'pretrained': False,
